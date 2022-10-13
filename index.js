857b1bd3ab63513bbe9069373b0e1a9c40c718be
Kevin Karneboge <kkarneboge@gmail.com>
2022-10-13 23:54:37 +0000

    made create

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+
+
 const data = require('./data.json');
 const fs = require('fs');
+const command = process.argv[2]
+const input = process.argv[3]
+let nextId = data.nextId
 
-if (process.argv[2] === 'read') {
+if (command === 'read') {
   for (let property in data.notes) {
     console.log(`${property}: ${data.notes[property]}`);
   }
+}
+
+if (command === 'create'){
+  data.notes[nextId] = String(input);
+  data.nextId++;
+  nextId = data.nextId;
+console.log(data);
+ const json = JSON.stringify(data, null, 2);
+fs.writeFile('data2.json', json, (error)=> {if (error) throw error})
 }
